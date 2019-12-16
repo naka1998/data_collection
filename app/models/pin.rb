b0970fc7f2aed185fe5f557d2d5cc7a2e581a8b5
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-12-16 10:50:08 +0900

    削除時にエラー出るの修正

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,15 +1,15 @@
 class Pin < ApplicationRecord
   belongs_to :station
-  has_one :elevator
-  has_one :restroom
-  has_one :slope
-  has_one :step
-  
+  has_one :elevator, dependent: :destroy
+  has_one :restroom, dependent: :destroy
+  has_one :slope, dependent: :destroy
+  has_one :step, dependent: :destroy
+
   accepts_nested_attributes_for :elevator, allow_destroy: true
   accepts_nested_attributes_for :restroom, allow_destroy: true
   accepts_nested_attributes_for :slope, allow_destroy: true
   accepts_nested_attributes_for :step, allow_destroy: true
-  
+
   enum data_type:{
     elevator: 1,
     restroom: 2,
